323f4d549c06b8db62cd916c13627fe62e18fa1f
jonquet <matthieu.jonquet@acseo-conseil.fr>
2025-06-26 10:32:35 +0200

    chore(symfony7): add new version on composer require

--- Form/Extension/FieldTypeHelpExtension.php
+++ Form/Extension/FieldTypeHelpExtension.php
@@ -26,7 +26,7 @@ class FieldTypeHelpExtension extends AbstractTypeExtension
      * @param FormBuilderInterface $builder
      * @param array                $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->setAttribute('help', $options['help']);
     }
@@ -36,7 +36,7 @@ class FieldTypeHelpExtension extends AbstractTypeExtension
      * @param FormInterface $form
      * @param array         $options
      */
-    public function buildView(FormView $view, FormInterface $form, array $options)
+    public function buildView(FormView $view, FormInterface $form, array $options): void
     {
         $view->vars['help'] = $options['help'];
     }
@@ -44,7 +44,7 @@ class FieldTypeHelpExtension extends AbstractTypeExtension
     /**
      * @param OptionsResolver $resolver
      */
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults(array(
             'help' => null,
